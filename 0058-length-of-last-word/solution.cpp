54ae9a223a43a5ea4af3dfcdaadf9f09c30f8d94
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2024-10-02 07:19:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (99.99%)

--- 0058-length-of-last-word/solution.cpp
+++ 0058-length-of-last-word/solution.cpp
@@ -1,16 +1,16 @@
  class Solution {
  public:
      int lengthOfLastWord(string s) {
-         int len = 0;
-         bool inWord = false;
-         for (int i = s.size() - 1; i >= 0; i--) {
-             if (s[i] != ' ') {
-                 len++;
-                 inWord = true;
-             } else if (inWord) {
-                 break;
-             }
-         }
-         return len;
+        int length = 0;
+        bool inword = false;
+        for (int i = s.length()-1; i>=0; i--){
+            if (s[i] != ' '){
+                length ++;
+                inword = true;
+            }else if(inword){
+                break;
+            }
+        }
+        return length;
      }
 };
