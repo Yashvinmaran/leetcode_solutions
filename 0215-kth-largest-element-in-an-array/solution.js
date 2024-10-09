801692c6402bae1ad01f5e85ee97e68676c50d4a
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2024-10-09 10:27:39 +0000

    Sync LeetCode submission Runtime - 226 ms (5.14%), Memory - 73.4 MB (20.55%)

---
+++ 0215-kth-largest-element-in-an-array/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    let minHeap = new MinPriorityQueue({ priority: x => x });
+
+    for (let num of nums) {
+        minHeap.enqueue(num);
+        if (minHeap.size() > k) {
+            minHeap.dequeue();
+        }
+    }
+
+    return minHeap.front().element;
+};
+
