aceb8f807ee8fc43193a508cd07169c663f72bf1
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2024-12-13 16:56:07 +0000

    Sync LeetCode submission Runtime - 6 ms (15.48%), Memory - 16.3 MB (5.20%)

---
+++ 1741-sort-array-by-increasing-frequency/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int, int> freqMap;
+        for (int num : nums) {
+            freqMap[num]++;
+        }
+
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if (freqMap[a] == freqMap[b]) {
+                return a > b;
+            }
+            return freqMap[a] < freqMap[b];
+        });
+
+        return nums;
+    }
+};
+
