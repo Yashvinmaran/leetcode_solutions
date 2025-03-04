a421634810982b5efb0d052659f166b7d737f657
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-03-04 15:56:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (65.35%)

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        for (;n>0;n=n/3){
+            if(n%3 == 2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
