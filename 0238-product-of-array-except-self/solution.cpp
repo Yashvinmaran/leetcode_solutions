ba7fffe8e07f643e885bb8cbc95bc3cbc8082f70
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-03-10 16:47:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40 MB (96.00%)

---
+++ 0238-product-of-array-except-self/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n);
+        
+        for (int i = 0, prefix = 1; i < n; i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        for (int i = n - 1, suffix = 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return answer;
+    }
+};
+
