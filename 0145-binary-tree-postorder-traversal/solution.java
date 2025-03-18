664da641e8dd9149d5b72e301d84282a4cf0217b
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-03-18 17:58:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.8 MB (99.97%)

---
+++ 0145-binary-tree-postorder-traversal/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer>arr = new ArrayList<>();
+        postorder(root, arr);
+        return arr;
+    }
+
+    private void postorder(TreeNode root, ArrayList<Integer>arr){
+        if (root == null)return;
+        postorder(root.left, arr);
+        postorder(root.right, arr);
+        arr.add(root.val);
+    }
+}
