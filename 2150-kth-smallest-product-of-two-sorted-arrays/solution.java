ba1e1f1afa896560ea26f4a70f89319daac5e4cf
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-03-31 17:47:08 +0000

    Sync LeetCode submission Runtime - 768 ms (26.47%), Memory - 56 MB (100.00%)

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/solution.java
@@ -0,0 +1,44 @@
+class Solution {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = (long) -1e10, right = (long) 1e10;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countSmallerOrEqual(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private long countSmallerOrEqual(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+        for (int num1 : nums1) {
+            if (num1 >= 0) {
+                int left = 0, right = nums2.length;
+                while (left < right) {
+                    int mid = left + (right - left) / 2;
+                    if ((long) num1 * nums2[mid] <= target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    }
+                }
+                count += left;
+            } else {
+                int left = 0, right = nums2.length;
+                while (left < right) {
+                    int mid = left + (right - left) / 2;
+                    if ((long) num1 * nums2[mid] <= target) {
+                        right = mid;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+                count += nums2.length - left;
+            }
+        }
+        return count;
+    }
+}
