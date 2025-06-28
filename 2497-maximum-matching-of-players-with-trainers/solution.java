456972887b02a62a1da5e4579c03247d2da31c59
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-06-28 05:35:55 +0000

    Sync LeetCode submission Runtime - 25 ms (98.42%), Memory - 65.9 MB (99.93%)

---
+++ 2497-maximum-matching-of-players-with-trainers/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        int n1 = players.length;
+        int n2 = trainers.length;
+
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i = 0, j = 0, c = 0;
+        while(i < n1 && j < n2){
+            if(players[i] <= trainers[j]){
+                c++;
+                i++;
+                j++;
+            }else if(players[i] > trainers[j])j++;
+        }
+        return c;
+    }
+}
