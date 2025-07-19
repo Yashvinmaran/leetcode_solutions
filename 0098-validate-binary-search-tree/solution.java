7cd5ecefd6639b10c366e420efacbf34527e1e0b
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-07-19 04:53:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.2 MB (99.84%)

--- 0098-validate-binary-search-tree/solution.java
+++ 0098-validate-binary-search-tree/solution.java
@@ -1,34 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private TreeNode prev = null;
-    
-    public boolean isValidBST(TreeNode root) {
-        return inorder(root);
+    private Integer prev = null;
+    private boolean helper(TreeNode root) {
+        if (root == null) return true;
+
+        if (!helper(root.left)) return false;
+
+        if (prev != null && root.val <= prev) return false;
+        prev = root.val;
+
+        if (!helper(root.right)) return false;
+
+        return true;
     }
 
-    private boolean inorder(TreeNode node) {
-        if (node == null) return true;
-        
-        if (!inorder(node.left)) return false;
-        
-        if (prev != null && node.val <= prev.val) return false;
-        prev = node;
-        
-        return inorder(node.right);
+    public boolean isValidBST(TreeNode root) {
+        return helper(root);
     }
 }
 
