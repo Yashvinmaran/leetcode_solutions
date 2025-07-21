4c915695af385788cf6b51a023a8a0de06821596
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-07-21 09:51:33 +0000

    Sync LeetCode submission Runtime - 33 ms (77.43%), Memory - 45.8 MB (100.00%)

---
+++ 1302-delete-characters-to-make-fancy-string/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+
+        result.append(s.charAt(0));
+
+        for (int i = 1; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            char prev = s.charAt(i - 1);
+
+            if (curr == prev) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count <= 2) {
+                result.append(curr);
+            }
+        }
+
+        return result.toString();
+    }
+}
+
