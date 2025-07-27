e7a6c85c06a07d68c3446d03df03422d0e292f94
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-07-27 16:29:14 +0000

    Sync LeetCode submission Runtime - 2 ms (20.81%), Memory - 41.7 MB (100.00%)

---
+++ 2316-count-hills-and-valleys-in-an-array/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        List<Integer> cleaned = new ArrayList<>();
+        cleaned.add(nums[0]);
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                cleaned.add(nums[i]);
+            }
+        }
+        int count = 0;
+        for (int i = 1; i < cleaned.size() - 1; i++) {
+            int prev = cleaned.get(i - 1);
+            int curr = cleaned.get(i);
+            int next = cleaned.get(i + 1);
+            
+            if ((prev < curr && curr > next) || (prev > curr && curr < next)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
