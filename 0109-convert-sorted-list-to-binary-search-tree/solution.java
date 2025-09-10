e103ae0fca4be530e050d830e46509c9a5885c7f
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-09-10 14:00:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.7 MB (100.00%)

---
+++ 0109-convert-sorted-list-to-binary-search-tree/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+
+     private static ListNode findMid(ListNode head) {
+        
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        prev.next = null;
+
+        return slow;
+    }
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) return null;
+        else if (head.next == null) return new TreeNode(head.val);
+
+        ListNode mid = findMid(head);
+        TreeNode root = new TreeNode(mid.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(mid.next);
+
+        return root;
+    }
+}
