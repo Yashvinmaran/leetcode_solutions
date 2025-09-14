ef2344e2c8d36711e9c9d268da1625a98c862222
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-09-14 16:52:07 +0000

    Sync LeetCode submission Runtime - 8 ms (68.98%), Memory - 44.6 MB (100.00%)

---
+++ 0981-delete-columns-to-make-sorted/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int m = strs.length;
+        int n = strs[0].length();
+        int c = 0;
+        
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < m; row++) {
+                if (strs[row].charAt(col) < strs[row - 1].charAt(col)) {
+                    c++;
+                    break;
+                }
+            }
+        }
+        return c;
+    }
+}
+
