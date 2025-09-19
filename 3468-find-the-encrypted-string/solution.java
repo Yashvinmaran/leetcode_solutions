4852a5adc1b7af02d7868043de53f899b4bbfbc5
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-09-19 17:44:51 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 42.8 MB (100.00%)

---
+++ 3468-find-the-encrypted-string/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        int n = s.length();
+        StringBuilder sb = new StringBuilder();
+        int c = k;
+
+        for (int i = 0; i<n; i++){
+            int idx = (i + k) % n;
+            sb.append(s.charAt(idx));
+        }
+        return sb.toString();
+    }
+}
