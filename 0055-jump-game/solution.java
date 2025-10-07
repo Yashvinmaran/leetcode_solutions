7afd1b965b75fe9d37bcac598a0c75f6f6a5565e
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-10-07 06:53:51 +0000

    Sync LeetCode submission Runtime - 2 ms (88.83%), Memory - 46.1 MB (99.97%)

--- 0055-jump-game/solution.java
+++ 0055-jump-game/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > maxReach) return false;
-            maxReach = Math.max(maxReach, i + nums[i]);
+        int n = nums.length;
+        int max = 0;
+        for(int i = 0;i<n;i++){
+            if(max<i) return false;
+            max = Math.max(max, i+nums[i]);
         }
         return true;
     }
 }
-
