acf77d76cac0a62271c4c416868aff21405e5bf2
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-10-24 16:14:42 +0000

    Sync LeetCode submission Runtime - 1 ms (86.75%), Memory - 42.9 MB (99.46%)

---
+++ 2093-check-if-string-is-a-prefix-of-array/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String str : words){
+            sb.append(str);
+            if(s.equals(sb.toString()))return true;
+        }
+        return false;
+    }
+}
