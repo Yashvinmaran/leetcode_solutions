15ed26e333c280582f3de140a7923f148200dce8
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-12-14 13:21:53 +0000

    Sync LeetCode submission Runtime - 3 ms (92.60%), Memory - 45.4 MB (22.57%)

---
+++ 0496-next-greater-element-i/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        while (!stack.isEmpty()) {
+            nextGreater.put(stack.pop(), -1);
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreater.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
+
