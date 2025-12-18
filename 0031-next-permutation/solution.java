7205d8d6d206fe6012766534748a8f472341b876
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-12-18 10:10:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.5 MB (83.90%)

--- 0031-next-permutation/solution.java
+++ 0031-next-permutation/solution.java
@@ -2,36 +2,25 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i+1])i--;
 
-        // Step 1: Find first decreasing element from the end
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        // Step 2: If found, find the element just greater than nums[i] to the right
-        if (i >= 0) {
+        if(i >= 0){
             int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            // Swap nums[i] and nums[j]
+            while(nums[j] <= nums[i])j--;
             swap(nums, i, j);
         }
-
-        // Step 3: Reverse the suffix starting at i+1
         reverse(nums, i + 1, n - 1);
     }
 
-    private void swap(int[] nums, int i, int j) {
+    private void swap(int[] nums, int i, int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            swap(nums, left++, right--);
+    private void reverse(int[] nums, int i, int j){
+        while(i < j){
+            swap(nums, i++, j--);
         }
     }
 }
-
