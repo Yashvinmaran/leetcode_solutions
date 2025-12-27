4c77dedf34470f3e0323c3c757152711c3f720fd
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2025-12-27 18:12:00 +0000

    Sync LeetCode submission Runtime - 8 ms (91.98%), Memory - 46.9 MB (52.78%)

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k == 1) return 0;
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE; 
+        int i = 0; 
+        int j = k - 1;
+        while( j < nums.length){
+                int temp = nums[j] - nums[i];
+                if(temp < diff) diff = temp;
+                i++;
+                j++;
+            }
+        
+        return diff;
+    }
+}
