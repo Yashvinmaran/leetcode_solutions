c1aad1b5beecc95684a06d181d3e8d225b2575a1
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-02-11 07:44:14 +0000

    Sync LeetCode submission Runtime - 3 ms (64.93%), Memory - 47.5 MB (77.90%)

--- 0645-set-mismatch/solution.java
+++ 0645-set-mismatch/solution.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int duplicate = -1, missing = -1;
-
-        for (int i = 0; i < nums.length; i++) {
-            int val = Math.abs(nums[i]);
-            if (nums[val - 1] < 0) {
-                duplicate = val;
+        
+        for (int num : nums) {
+            int index = Math.abs(num) - 1;
+            if (nums[index] < 0) {
+                duplicate = Math.abs(num);
             } else {
-                nums[val - 1] *= -1;
+                nums[index] *= -1;
             }
         }
-
+        
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
                 missing = i + 1;
-            } else {
-                nums[i] = Math.abs(nums[i]);
+                break;
             }
         }
-        return new int[] { duplicate, missing };
+        
+        return new int[]{duplicate, missing};
     }
 }
 
