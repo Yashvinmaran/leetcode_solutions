fd266dddeda3dcda4da7b8987d5ee127367e3e8a
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-02-15 04:52:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.9 MB (80.78%)

--- 0067-add-binary/solution.cpp
+++ 0067-add-binary/solution.cpp
@@ -1,27 +1,20 @@
 class Solution {
-    string addBinaryStrings(string a, string b) {
-        string result = "";
-        int carry = 0;
-
-        while (a.length() < b.length()) a = '0' + a;
-        while (b.length() < a.length()) b = '0' + b;
-
-        for (int i = a.length() - 1; i >= 0; i--) {
-            int bitA = a[i] - '0';
-            int bitB = b[i] - '0';
-            int sum = bitA + bitB + carry;
-
-            result = char((sum % 2) + '0') + result;
-            carry = sum / 2;
-        }
-        if (carry) result = '1' + result;
-
-        return result;
-    }
-
 public:
     string addBinary(string a, string b) {
-        return addBinaryStrings(a, b);
+        string res;
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            if (i >= 0) carry += a[i--] - '0';
+            if (j >= 0) carry += b[j--] - '0';
+
+            res += carry % 2 + '0';
+            carry /= 2;
+        }
+
+        reverse(begin(res), end(res));
+        return res;        
     }
 };
-
