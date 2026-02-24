93d49fb172d4cf15974a0ae6b755c597ce317470
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-02-24 05:29:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (29.32%)

--- 1079-sum-of-root-to-leaf-binary-numbers/solution.java
+++ 1079-sum-of-root-to-leaf-binary-numbers/solution.java
@@ -1,20 +1,18 @@
 class Solution {
-    public static int helper(TreeNode root,int curr){
-        curr=curr*2+root.val;
-        if(root.left==null&&root.right==null){
-            return curr;
+    private int helper(TreeNode root, int curr){
+        if(root == null)return 0;
+        curr = curr*2 + root.val;
+        if(root.left == null && root.right == null) return curr;
+        int ans = 0;
+        if(root.left != null) {
+            ans += helper(root.left, curr);
         }
-        int ans=0;
-        if(root.left!=null){
-            ans+=helper(root.left,curr);
+        if(root.right != null){
+            ans +=  helper(root.right, curr);
         }
-        if(root.right!=null){
-            ans+=helper(root.right,curr);
-        }  
-        return ans; 
+        return ans;
     }
     public int sumRootToLeaf(TreeNode root) {
-        
-        return helper(root,0);
+        return helper(root, 0);
     }
 }
