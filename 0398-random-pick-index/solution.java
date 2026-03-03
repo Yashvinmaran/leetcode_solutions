fdd7fe541e13ad8a7eec3f3c0b4a3458be506ba2
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-03-03 13:59:19 +0000

    Sync LeetCode submission Runtime - 1505 ms (26.24%), Memory - 60.8 MB (86.39%)

---
+++ 0398-random-pick-index/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    private int[] nums;
+    private Random rand = new Random();
+
+    public Solution(int[] nums) {
+        this.nums = nums;
+    }
+    
+    public int pick(int target) {
+        int count = 0;
+        int result = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                count++;
+               if (rand.nextInt(count) == 0) {
+                    result = i;
+                }
+            }
+        }
+        return result;
+    }
+}
+
