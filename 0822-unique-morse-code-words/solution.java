2157b471c367bc318a4da3689b1b444051f0af27
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-03-10 06:27:17 +0000

    Sync LeetCode submission Runtime - 4 ms (9.77%), Memory - 43.3 MB (72.15%)

---
+++ 0822-unique-morse-code-words/solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character, String> map = new HashMap<>();
+        map.put('a',".-");
+        map.put('b',"-...");
+        map.put('c',"-.-.");
+        map.put('d',"-..");
+        map.put('e',".");
+        map.put('f',"..-.");
+        map.put('g',"--.");
+        map.put('h',"....");
+        map.put('i',"..");
+        map.put('j',".---");
+        map.put('k',"-.-");
+        map.put('l',".-..");
+        map.put('m',"--");
+        map.put('n',"-.");
+        map.put('o',"---");
+        map.put('p',".--.");
+        map.put('q',"--.-");
+        map.put('r',".-.");
+        map.put('s',"...");
+        map.put('t',"-");
+        map.put('u',"..-");
+        map.put('v',"...-");
+        map.put('w',".--");
+        map.put('x',"-..-");
+        map.put('y',"-.--");
+        map.put('z',"--..");
+
+        Set<String> set = new HashSet<>();
+        for (String word : words){
+            String str = "";
+            for (char c : word.toCharArray()){
+                str += map.get(c);
+            }
+            set.add(str);
+        }
+        return set.size();
+    }
+}
