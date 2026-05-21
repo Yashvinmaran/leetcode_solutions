59006097c3ec33c84c75e90bbd5f837032a66ef4
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-05-21 13:22:37 +0000

    Sync LeetCode submission Runtime - 69 ms (48.16%), Memory - 67.9 MB (90.15%)

--- 0215-kth-largest-element-in-an-array/solution.java
+++ 0215-kth-largest-element-in-an-array/solution.java
@@ -1,7 +1,18 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        int length = nums.length;
-        return nums[length-k];
+        // Create a Min-Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int n : nums) {
+            pq.add(n);
+            // Keep the heap size strictly at k
+            if (pq.size() > k) {
+                pq.poll(); // Remove the smallest element
+            }
+        }
+        
+        // The root of the Min-heap is now the kth largest element
+        return pq.peek();
     }
 }
+
