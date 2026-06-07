c541a2cc72cc7d330eeb89e1a259fca39e606e87
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-06-07 07:05:05 +0000

    Sync LeetCode submission Runtime - 7 ms (99.88%), Memory - 82.2 MB (76.20%)

--- 2306-create-binary-tree-from-descriptions/solution.java
+++ 2306-create-binary-tree-from-descriptions/solution.java
@@ -1,45 +1,28 @@
 class Solution {
+
+    private static final TreeNode[] nodes = new TreeNode[100001];
+    
     public TreeNode createBinaryTree(int[][] descriptions) {
-        Set<Integer> childrenSet = new HashSet<>();
-        Map<Integer, int[]> childrenHashmap = new HashMap<>();
+        for (int[] x : descriptions) {
+            nodes[x[0]] = null;
+        }
+        for (int[] x : descriptions) {
+            nodes[x[1]] = new TreeNode(x[1]);
+        }
 
-        for (int[] desc : descriptions) {
-            int parent = desc[0];
-            int child = desc[1];
-            boolean isLeft = desc[2] == 1;
+        TreeNode root = null;
+        for (int[] x : descriptions) {
 
-            childrenHashmap.putIfAbsent(parent, new int[]{-1, -1});
-            childrenSet.add(child);
+            if (nodes[x[0]] == null) {
+                root = nodes[x[0]] = new TreeNode(x[0]);
+            }
 
-            if (isLeft) {
-                childrenHashmap.get(parent)[0] = child;
+            if (x[2] == 1) {
+                nodes[x[0]].left = nodes[x[1]];
             } else {
-                childrenHashmap.get(parent)[1] = child;
+                nodes[x[0]].right = nodes[x[1]];
             }
         }
-
-        int headNodeVal = 0;
-        for (int parent : childrenHashmap.keySet()) {
-            if (!childrenSet.contains(parent)) {
-                headNodeVal = parent;
-                break;
-            }
-        }
-
-        return constructTree(headNodeVal, childrenHashmap);
-    }
-
-    private TreeNode constructTree(int curNodeVal, Map<Integer, int[]> childrenHashmap) {
-        TreeNode newNode = new TreeNode(curNodeVal);
-        if (childrenHashmap.containsKey(curNodeVal)) {
-            int[] children = childrenHashmap.get(curNodeVal);
-            if (children[0] != -1) {
-                newNode.left = constructTree(children[0], childrenHashmap);
-            }
-            if (children[1] != -1) {
-                newNode.right = constructTree(children[1], childrenHashmap);
-            }
-        }
-        return newNode;
+        return root;
     }
 }
