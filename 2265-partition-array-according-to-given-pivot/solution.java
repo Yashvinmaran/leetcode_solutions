f7f26c820f287e1bcfa8b6fdaa6198eb24803980
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-06-08 11:06:47 +0000

    Sync LeetCode submission Runtime - 5 ms (72.59%), Memory - 159.9 MB (55.44%)

--- 2265-partition-array-according-to-given-pivot/solution.java
+++ 2265-partition-array-according-to-given-pivot/solution.java
@@ -1,28 +1,37 @@
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-        int n = nums.length;
-        int[] ans = new int[n];
 
-        int idx = 0;
-        for (int num : nums) {
-            if (num < pivot) {
-                ans[idx++] = num;
+    public int[] pivotArray(int[] arr, int pivotValue) {
+        int n = arr.length;
+        int[] result = new int[n];
+        
+        // Three independent pointers to fill the result array
+        int lessPtr = 0;
+        int equalPtr = 0;
+        int greaterPtr = 0;
+
+        // Step 1: Count how many elements are strictly smaller or equal to the pivot.
+        // This calculates exactly where each section should start in the new array.
+        for (int num : arr) {
+            if (num < pivotValue) {
+                equalPtr++; // Shifts the start of the 'equal' section
+                greaterPtr++; // Shifts the start of the 'greater' section
+            } else if (num == pivotValue) {
+                greaterPtr++; // Shifts the start of the 'greater' section
             }
         }
 
-        for (int num : nums) {
-            if (num == pivot) {
-                ans[idx++] = num;
+        // Step 2: Fill the result array sequentially to maintain original relative order.
+        for (int num : arr) {
+            if (num < pivotValue) {
+                result[lessPtr++] = num;
+            } else if (num == pivotValue) {
+                result[equalPtr++] = num;
+            } else {
+                result[greaterPtr++] = num;
             }
         }
 
-        for (int num : nums) {
-            if (num > pivot) {
-                ans[idx++] = num;
-            }
-        }
-
-        return ans;
+        return result;
     }
 }
 
