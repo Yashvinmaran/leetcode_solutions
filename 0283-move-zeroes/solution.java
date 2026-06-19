79d235b889cbfb105e32a42bf8afd949b6191124
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-06-19 14:51:27 +0000

    Sync LeetCode submission Runtime - 2 ms (92.17%), Memory - 47.6 MB (87.98%)

--- 0283-move-zeroes/solution.java
+++ 0283-move-zeroes/solution.java
@@ -1,31 +1,13 @@
 class Solution {
-    public int seti(int i, int[] arr){
-        for (int j = i; j<arr.length; j++){
-            if(arr[j] == 0)return j;
-        }
-        return arr.length;
-    }
-    public int setj(int i, int[] arr){
-        boolean flag = false;
-        for (int j = i; j<arr.length; j++){
-            if(arr[j] == 0 && !flag)flag = true;;
-            if(arr[j] != 0 && flag)return j;
-        }
-        return arr.length;
-    }
     public void moveZeroes(int[] nums) {
-        int n = nums.length;
-        int i = seti(0, nums);
-        int j = setj(0, nums);
-        while(i<n && j<n){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i = seti(i, nums); 
-            j = setj(j, nums);
-        }
-        for (int a : nums){
-            System.out.print(a + " ");
+        int idx = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] == 0)continue;
+            int temp = nums[idx];
+            nums[idx] = nums[i];
+            nums[i] = temp;
+            idx++;
         }
     }
 }
