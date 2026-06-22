2fbe9c6a8de95fe24fd4a552ab3277d60fb7f11a
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-06-22 16:22:40 +0000

    Sync LeetCode submission Runtime - 24 ms (77.10%), Memory - 49 MB (33.79%)

--- 0560-subarray-sum-equals-k/solution.java
+++ 0560-subarray-sum-equals-k/solution.java
@@ -1,16 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int prefix = 0;
         int cnt = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        int prefixSum = 0;
+        
+        // Map to store (prefix_sum -> frequency)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        // Base case: a prefix sum of 0 has occurred once (for subarrays starting at index 0)
         map.put(0, 1);
+        
         for (int i = 0; i < nums.length; i++) {
-            prefixSum += nums[i];
-            int remove = prefixSum - k;
-            cnt += map.getOrDefault(remove, 0);
-            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+            prefix += nums[i];
+            
+            // If (prefix - k) exists in the map, it means we found valid subarrays
+            if (map.containsKey(prefix - k)) {
+                cnt += map.get(prefix - k);
+            }
+            
+            // Record the current prefix sum into the map
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
-
+        
         return cnt;
     }
 }
