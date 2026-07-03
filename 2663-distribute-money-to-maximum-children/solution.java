01320c6bd444e4afe0ab79adfd8a3f9a19b04bac
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-07-03 11:54:27 +0000

    Sync LeetCode submission Runtime - 2 ms (97.36%), Memory - 43.5 MB (92.74%)

---
+++ 2663-distribute-money-to-maximum-children/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int distMoney(int money, int children) {
+        if(money < children)return -1;
+
+        if(money == children*8)return children;
+
+        if(money > children*8)return children - 1;
+
+        int temp = money - children;
+        int result = temp / 7;
+        int rem = temp % 7;
+
+        if(children - result == 1 && rem == 3) result--;
+
+        return result;
+    }
+}
