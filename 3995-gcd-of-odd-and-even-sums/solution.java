abfd96ae2e3147a06b93dd799f3038bba24eb375
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-07-15 04:46:35 +0000

    Sync LeetCode submission Runtime - 1 ms (76.42%), Memory - 42.7 MB (40.81%)

---
+++ 3995-gcd-of-odd-and-even-sums/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    private static int gcd(int num1, int num2){
+        while (num2 != 0) {
+            int remainder = num1 % num2;
+            num1 = num2;
+            num2 = remainder;
+        }
+        return num1;
+    }
+
+    public int gcdOfOddEvenSums(int n) {
+       
+        // return gcd(n * (n + 1), n * n);
+        return n;
+    }
+}
