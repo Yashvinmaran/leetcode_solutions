7ba1ba75ef2de4dd94750ea080982d5314de6c62
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-08-09 07:51:42 +0000

    Sync LeetCode submission Runtime - 9 ms (51.10%), Memory - 45.4 MB (35.29%)

--- 1240-stone-game-ii/solution.java
+++ 1240-stone-game-ii/solution.java
@@ -1,31 +1,3 @@
-// class Solution {
-
-//     private static int solveHelper(int[] piles, int[] suffixSum, int left, int m) {
-//         if (left + 2 * m >= piles.length) {
-//             return suffixSum[left];
-//         }
-//         int res = Integer.MIN_VALUE;
-//         for (int x = 1; x <= 2 * m; x++) {
-//             res = Math.max(res, suffixSum[left] - solveHelper(piles, suffixSum, left + x, Math.max(m, x)));
-//         }
-//         return res;
-//     }
-
-//     public int stoneGameII(int[] piles) {
-//         int n = piles.length;
-//         int[] suffixSum = new int[n];
-//         suffixSum[n - 1] = piles[n - 1];
-//         for (int i = n - 2; i >= 0; i--) {
-//             suffixSum[i] = suffixSum[i + 1] + piles[i];
-//         }
-//         return solveHelper(piles, suffixSum, 0, 1);
-//     }
-// }
-
-
-
-
-
 class Solution {
     
     private static int solve(int[] piles, int person, int idx, int m, int[][][] dp) {
@@ -66,4 +38,3 @@ class Solution {
         return solve(piles, 1, 0, 1, dp);
     }
 }
-
