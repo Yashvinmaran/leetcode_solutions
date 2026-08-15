5486cd01525506088399898e44dc0a4779a51f44
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-08-15 17:13:04 +0000

    Sync LeetCode submission Runtime - 36 ms (37.77%), Memory - 48 MB (26.33%)

--- 2317-count-collisions-on-a-road/solution.java
+++ 2317-count-collisions-on-a-road/solution.java
@@ -1,16 +1,30 @@
-class Solution {
-    public int countCollisions(String dir) {
+class Solution { 
+    public int countCollisions(String directions) { 
+        int collide = 0; 
+        int left = 0; 
+        boolean flag = false; 
+        Stack<Character> st = new Stack<>(); 
         
-        int n = dir.length();
-        int left = 0, right = n - 1;
-        int ans = 0;
-
-        while(left < n && dir.charAt(left) == 'L')left++;
-        while(right >= 0 && dir.charAt(right) == 'R')right--;
-
-        for (int d = left; d <= right; d++){
-            if(dir.charAt(d) != 'S')ans++;
-        }
-        return ans;
-    }
+        for (char c : directions.toCharArray()){ 
+            if(c == 'R') {
+                st.add(c); 
+            } else if(c == 'L'){ 
+                left = flag ? left + 1 : left; 
+                if(!st.isEmpty()){ 
+                    collide += st.size() + 1; 
+                    left = flag ? left - 1 : left; 
+                    st.clear(); 
+                    flag = true; 
+                } 
+            } else { 
+                flag = true; 
+                if(!st.isEmpty()){ 
+                    collide += st.size(); 
+                    st.clear(); 
+                } 
+            } 
+        } 
+        return collide + left; 
+    } 
 }
+
