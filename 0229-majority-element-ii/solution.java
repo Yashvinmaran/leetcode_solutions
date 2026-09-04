574c272011c6e23e06584a6f51263758edcd8141
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-04 10:19:46 +0000

    Sync LeetCode submission Runtime - 3 ms (73.62%), Memory - 52.7 MB (29.83%)

--- 0229-majority-element-ii/solution.java
+++ 0229-majority-element-ii/solution.java
@@ -1,20 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length / 3;
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
+        
+        List<Integer> ans = new ArrayList<>();
 
-        for (int num : nums) {
-            mpp.put(num, mpp.getOrDefault(num, 0) + 1);
-        }
+        int curr1 = Integer.MIN_VALUE;
+        int curr2 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int cnt2 = 0;
 
-        for (int key : mpp.keySet()) {
-            if (mpp.get(key) > n) {
-                list.add(key);
+        for (int i = 0; i < nums.length; i++){
+            if(cnt1 == 0 && nums[i] != curr2){
+                curr1 = nums[i];
+                cnt1++;
+            }
+            else if(nums[i] == curr1)cnt1++;
+            
+            else if(cnt2 == 0 && nums[i] != curr1){
+                curr2 = nums[i];
+                cnt2++;
+            }
+            else if(nums[i] == curr2)cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
 
-        return list;
+        int c1 = 0, c2 = 0;
+        for (int i = 0; i < nums.length; i++){
+            if(curr1 == nums[i])c1++;
+            if(curr2 == nums[i])c2++;
+        }
+        int mini = (int)(nums.length / 3) + 1;
+        if(c1 >= mini)ans.add(curr1);
+        if(c2 >= mini)ans.add(curr2);
+
+        Collections.sort(ans);
+        return ans;
     }
 }
-
