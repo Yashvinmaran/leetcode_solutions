8d32af8cbb2dbf5502f0775241658710700f57b9
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-06 15:27:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 47.4 MB (32.39%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,8 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int sum = n*(n + 1) /2;
-        for (int i : nums)sum -= i;
-        return sum;
+
+       int max = nums.length;
+       int totalSum = 0;
+
+       for (int i = 0; i < nums.length; i++){
+        // if(nums[i] > max)max = nums[i];
+        totalSum += nums[i];
+       }
+
+       return (max * (max + 1)/2) - totalSum;
     }
 }
