34d504de149c75d54863d68219c76bf81bf555d5
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-07 11:00:10 +0000

    Sync LeetCode submission Runtime - 4 ms (92.33%), Memory - 82.8 MB (58.90%)

--- 0287-find-the-duplicate-number/solution.java
+++ 0287-find-the-duplicate-number/solution.java
@@ -1,34 +1,21 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length - 1;
-        int duplicate = 0;
 
-        // Check each bit position (0 to 31)
-        for (int bit = 0; bit < 32; bit++) {
-            int countNums = 0;
-            int countRange = 0;
+        int slow = nums[0];
+        int fast = nums[0];
 
-            // Count bits in nums array
-            for (int num : nums) {
-                if ((num & (1 << bit)) != 0) {
-                    countNums++;
-                }
-            }
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
 
-            // Count bits in range [1..n]
-            for (int i = 1; i <= n; i++) {
-                if ((i & (1 << bit)) != 0) {
-                    countRange++;
-                }
-            }
-
-            // If extra bits found, this bit belongs to duplicate
-            if (countNums > countRange) {
-                duplicate |= (1 << bit);
-            }
+        slow = nums[0];
+        
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return duplicate;
+        return slow;
     }
 }
-
