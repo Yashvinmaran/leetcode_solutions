e14a52b16d3cad3c8c1ef39886ed0488548bedca
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-09 11:59:07 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 71.2 MB (58.98%)

---
+++ 0041-first-missing-positive/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        boolean checkOne = false;
+
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] == 1)checkOne = true;
+            if(nums[i] <= 0)nums[i] = 1;
+        }
+        if(!checkOne)return 1;
+
+        for (int i = 0; i < nums.length; i++){
+            int idx = Math.abs(nums[i]) - 1;
+            if(idx < nums.length && nums[idx] > 0)nums[idx] *= -1;
+        }
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] > 0)return i+1;
+        }
+
+        return nums.length + 1;
+    }
+}
