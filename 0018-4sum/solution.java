e85a61ed07c651d42f35a7ac69b4405163565f54
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-09 10:32:15 +0000

    Sync LeetCode submission Runtime - 20 ms (65.57%), Memory - 45.8 MB (62.78%)

--- 0018-4sum/solution.java
+++ 0018-4sum/solution.java
@@ -1,41 +1,36 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        int n = nums.length;
         Arrays.sort(nums);
-
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int left = j + 1;
-                int right = n - 1;
-
-                while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for (int a = 0; a < nums.length; a++) {
+            if (a > 0 && nums[a] == nums[a - 1]) continue;
+            
+            for (int i = a + 1; i < nums.length; i++) {
+                if (i > a + 1 && nums[i] == nums[i - 1]) continue;
+                
+                int j = i + 1;
+                int k = nums.length - 1;
+                
+                while (j < k) {
+                    long sum = (long) nums[a] + nums[i] + nums[j] + nums[k];
+                    
                     if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
-                        left++;
-                        right--;
+                        ans.add(new ArrayList<>(Arrays.asList(nums[a], nums[i], nums[j], nums[k])));
+                        j++;
+                        k--;
+                        
+                        while (j < k && nums[j] == nums[j - 1]) j++;
+                        while (j < k && nums[k] == nums[k + 1]) k--;
                     } else if (sum < target) {
-                        left++;
+                        j++; 
                     } else {
-                        right--;
+                        k--; 
                     }
                 }
             }
         }
-
-        return result;
+        return ans;
     }
 }
 
