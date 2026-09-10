4a8b103b2a4d6c4c6a910c0beb48a71f8fb2935f
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-10 14:49:26 +0000

    Sync LeetCode submission Runtime - 1 ms (95.99%), Memory - 47.8 MB (24.18%)

--- 0073-set-matrix-zeroes/solution.java
+++ 0073-set-matrix-zeroes/solution.java
@@ -1,25 +1,59 @@
+import java.util.Arrays;
+
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int r = matrix.length;
-        int c = matrix[0].length;
+
+    public void setZeroes(int[][] mat) {
+
+        int m = mat.length;
+        int n = mat[0].length;
         
-        boolean[] rows = new boolean[r];
-        boolean[] cols = new boolean[c];
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (matrix[i][j] == 0) {
-                    rows[i] = true;
-                    cols[j] = true;
+        boolean first_col_zero = false; 
+        boolean first_row_zero = false;
+
+        for (int i = 0; i < m; i++){
+            if(mat[i][0] == 0){
+                first_col_zero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++){
+            if(mat[0][i] == 0){
+                first_row_zero = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
                 }
             }
         }
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (rows[i] || cols[j]) {
-                    matrix[i][j] = 0;
+
+        for (int i = 1; i < m; i++){
+            if(mat[i][0] == 0){
+                Arrays.fill(mat[i], 0);
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            if(mat[0][i] == 0){
+                for (int j = 0; j < m; j++){
+                    mat[j][i] = 0;
                 }
+            }
+        }
+
+        if(first_row_zero){
+            Arrays.fill(mat[0], 0);
+        }
+
+        if(first_col_zero){
+            for (int i = 0; i < m; i++){ 
+                mat[i][0] = 0;
             }
         }
     }
