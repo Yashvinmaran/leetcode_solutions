8bbe7e3557b5863e27676e19edd6f8a05b55dd28
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-12 14:24:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (7.75%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,30 +1,25 @@
 class Solution {
+    private static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
     public void sortColors(int[] nums) {
         int left = 0;
-        int right = nums.length - 1;
         int mid = 0;
+        int right = nums.length - 1;
 
-        while (mid <= right) {
-            if (nums[mid] == 0) {
-                // Only XOR if indices are different to avoid resetting to 0
-                if (mid != left) {
-                    nums[mid] ^= nums[left];
-                    nums[left] ^= nums[mid];
-                    nums[mid] ^= nums[left];
-                }
+        while(mid <= right){
+            if(nums[mid] == 0){
+                swap(nums, mid, left);
                 left++;
                 mid++;
-            } else if (nums[mid] == 2) {
-                // Only XOR if indices are different
-                if (mid != right) {
-                    nums[mid] ^= nums[right];
-                    nums[right] ^= nums[mid];
-                    nums[mid] ^= nums[right];
-                }
-                
-                right--; 
-            } else {
+            }else if(nums[mid] == 1){
                 mid++;
+            }else {
+                swap(nums, mid, right);
+                right--;
             }
         }
     }
