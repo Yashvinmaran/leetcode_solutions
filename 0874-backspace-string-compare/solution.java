ed94eacb136a1d2612184b3a762c4cf3df59115b
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-12 15:09:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (67.11%)

--- 0874-backspace-string-compare/solution.java
+++ 0874-backspace-string-compare/solution.java
@@ -1,21 +1,48 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
+        int i = s.length() - 1;
+        int j = t.length() - 1;
 
-        for (char c : s.toCharArray()) {
-            if (c == '#') {
-                if (sb1.length() > 0) sb1.deleteCharAt(sb1.length() - 1);
-            } else sb1.append(c);
+        int skipS = 0;
+        int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j)) {
+                    return false;
+                }
+            } else if (i >= 0 || j >= 0) {
+                return false;
+            }
+
+            i--;
+            j--;
         }
 
-        for (char c : t.toCharArray()) {
-            if (c == '#') {
-                if (sb2.length() > 0) sb2.deleteCharAt(sb2.length() - 1);
-            } else sb2.append(c);
-        }
-
-        return sb1.toString().equals(sb2.toString());  
+        return true;
     }
 }
-
