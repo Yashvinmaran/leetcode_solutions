ab51a2a19d2b9268472f279278a62d85510f2597
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-15 15:25:26 +0000

    Sync LeetCode submission Runtime - 28 ms (32.28%), Memory - 47.4 MB (25.83%)

--- 0438-find-all-anagrams-in-a-string/solution.java
+++ 0438-find-all-anagrams-in-a-string/solution.java
@@ -1,25 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
 
-        List<Integer> ans = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
 
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        for(char c : p.toCharArray()){
-            freq1[c - 'a']++;
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
+        int required = map.size();
         int left = 0;
 
+        List<Integer> ans = new ArrayList<>();
+
         for (int right = 0; right < s.length(); right++) {
-            freq2[s.charAt(right) - 'a']++;
-            while(freq1[s.charAt(right) - 'a'] < freq2[s.charAt(right) - 'a']){
-                freq2[s.charAt(left) - 'a']--;
+            map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) - 1);
+            if (map.get(s.charAt(right)) == 0) {
+                required--;
+            }
+            while (required == 0 && left <= right) {
+                map.put(s.charAt(left), map.getOrDefault(s.charAt(left), 0) + 1);
+                if (map.get(s.charAt(left)) > 0) {
+                    required++;
+                }
+                if (right - left + 1 == p.length()) {
+                    ans.add(left);
+                }
                 left++;
             }
-
-            if((right - left + 1) == p.length())ans.add(left);
         }
 
         return ans;
