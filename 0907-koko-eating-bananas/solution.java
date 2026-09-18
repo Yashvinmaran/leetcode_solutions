ffe65d125252a51e10f6088dccd159c96b2c88a0
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-18 06:39:22 +0000

    Sync LeetCode submission Runtime - 11 ms (51.96%), Memory - 47.9 MB (57.72%)

--- 0907-koko-eating-bananas/solution.java
+++ 0907-koko-eating-bananas/solution.java
@@ -1,5 +1,7 @@
-class Solution { 
-    private static int calculate(int[] piles, int k){ 
+class Solution {
+
+    private static int eatingTimeAtSpeekOf(int[] piles, int k) {
+
         int sum = 0; 
         for (int pile : piles){ 
             if(pile % k == 0){ 
@@ -8,27 +10,26 @@ class Solution {
                 sum += (pile / k) + 1; 
             }
         } 
-        return sum; 
-    } 
+        return sum;
+    }
 
-    public int minEatingSpeed(int[] piles, int h) { 
-        int low = 1; 
-        int high = Integer.MAX_VALUE; 
-     
-        
-        int ans = high; 
-        while(low <= high){ 
-            int mid = low + (high - low) / 2; 
-            int check = calculate(piles, mid); 
-            
-            if(check <= h){ 
-                ans = mid; 
-                high = mid - 1; 
-            } else {
-                low = mid + 1;
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = Integer.MAX_VALUE;
+
+        int left = 1;
+        int right = max;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int time = eatingTimeAtSpeekOf(piles, mid);
+            if (time <= h) { 
+                max = mid;        
+                right = mid - 1;  
+            } else { 
+                left = mid + 1;   
             }
-        } 
-        return ans; 
-    } 
+        }
+        return max;
+    }
 }
-
