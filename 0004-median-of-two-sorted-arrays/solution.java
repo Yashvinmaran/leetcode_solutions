8418b67d5ca973a615115940563ce180575a7bce
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-20 11:36:15 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 49 MB (32.41%)

--- 0004-median-of-two-sorted-arrays/solution.java
+++ 0004-median-of-two-sorted-arrays/solution.java
@@ -1,32 +1,43 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int size1 = nums1.length;
-        int size2 = nums2.length;
-        int size = size1 + size2;
-        int[] newarr = new int[size];
+        
+        int m = nums1.length;
+        int n = nums2.length;
+        if(m > n)return findMedianSortedArrays(nums2, nums1);
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        int low = 0;
+        int high = m;
 
-        while (i < size1 && j < size2) {
-            if (nums1[i] < nums2[j]) {
-                newarr[k++] = nums1[i++];
-            } else {
-                newarr[k++] = nums2[j++];
+        int partition = (m + n + 1) / 2;
+
+        int len = m + n;
+
+        while(low <= high){
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+
+            int mid1 = (low + high) >> 1;
+            int mid2 = partition - mid1;
+
+            if(mid1 < m)r1 = nums1[mid1];
+            if(mid2 < n)r2 = nums2[mid2];
+            if(mid1 - 1 >= 0)l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0)l2 = nums2[mid2 - 1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(len % 2 == 1)return Math.max(l1,l2);
+                return (double)(Math.max(l1,l2) + Math.min(r1,r2)) / 2;
+            }else if(l1 > r2){
+                high = mid1 - 1;
+            }else{
+                low = mid1 + 1;
             }
         }
-        while (i < size1) {
-            newarr[k++] = nums1[i++];
-        }
-        while (j < size2) {
-            newarr[k++] = nums2[j++];
-        }
-
-        if (size % 2 == 0) {
-            return (newarr[size / 2 - 1] + newarr[size / 2]) / 2.0;
-        }
-        return newarr[size / 2];
+        return 0;
     }
 }
 
+// 1 2
+// 3 4
