36bcb82f75c5a1f46af5bf21c1d7757da21b201f
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-20 07:40:49 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 43.8 MB (81.16%)

--- 3811-reverse-degree-of-a-string/solution.java
+++ 3811-reverse-degree-of-a-string/solution.java
@@ -1,9 +1,14 @@
 class Solution {
+
     public int reverseDegree(String s) {
+        
         int sum = 0;
-        for (int i = 0; i<s.length(); i++){
-            sum += ((26 - (s.charAt(i) - 'a'))*(i+1));
+
+        for (int i = 0; i < s.length(); i++){
+            int pro = (26 - (s.charAt(i) - 'a'));
+            sum += (pro * (i + 1));
         }
+
         return sum;
     }
 }
