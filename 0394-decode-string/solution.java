ffbc354903653e98cf3d8cb0a66c256dc3c9bdac
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-22 11:16:02 +0000

    Sync LeetCode submission Runtime - 1 ms (83.68%), Memory - 42.9 MB (59.64%)

--- 0394-decode-string/solution.java
+++ 0394-decode-string/solution.java
@@ -1,30 +1,36 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> strStack = new Stack<>();
-        Stack<Integer> numStack = new Stack<>();
-        StringBuilder currStr = new StringBuilder();
-        int num = 0;
+       
+        Deque<Integer> counts = new ArrayDeque<>();
+        Deque<StringBuilder> resultStack = new ArrayDeque<>();
+        
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
 
         for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
-                num = num * 10 + (c - '0'); 
+                
+                k = k * 10 + (c - '0');
             } else if (c == '[') {
-                numStack.push(num);
-                strStack.push(currStr.toString());
-                num = 0;
-                currStr.setLength(0);
+                counts.push(k);
+                resultStack.push(currentString);
+                
+                currentString = new StringBuilder();
+                k = 0;
             } else if (c == ']') {
-                int repeatCount = numStack.pop();
-                String temp = currStr.toString();
-                currStr = new StringBuilder(strStack.pop());
-                while (repeatCount-- > 0) {
-                    currStr.append(temp);
+                StringBuilder decodedString = resultStack.pop();
+                int currentK = counts.pop();
+                
+                for (int i = 0; i < currentK; i++) {
+                    decodedString.append(currentString);
                 }
+                currentString = decodedString;
             } else {
-                currStr.append(c);
+                currentString.append(c);
             }
         }
 
-        return currStr.toString();
+        return currentString.toString();
     }
 }
+
