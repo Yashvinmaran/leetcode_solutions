7c4b7ebdcfa313542e0436c84ba56cb42e5b9c15
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-23 13:49:33 +0000

    Sync LeetCode submission Runtime - 5 ms (41.70%), Memory - 46.9 MB (46.58%)

--- 0023-merge-k-sorted-lists/solution.java
+++ 0023-merge-k-sorted-lists/solution.java
@@ -1,64 +1,30 @@
 class Solution {
-
-    public ListNode sort(ListNode head) {
-        if (head == null || head.next == null) return head;
-
-        ListNode mid = getMid(head);
-        ListNode right = mid.next;
-        mid.next = null;
-
-        ListNode leftSorted = sort(head);
-        ListNode rightSorted = sort(right);
-
-        return merge(leftSorted, rightSorted);
-    }
-
-    private ListNode getMid(ListNode head) {
-        ListNode slow = head, fast = head.next;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
         }
-        return slow;
-    }
 
-    private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a.val, b.val));
 
-        while (l1 != null && l2 != null) {
-            if (l1.val <= l2.val) {
-                temp.next = l1;
-                l1 = l1.next;
-            } else {
-                temp.next = l2;
-                l2 = l2.next;
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHeap.add(list);
             }
-            temp = temp.next;
         }
-        temp.next = (l1 != null) ? l1 : l2;
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                minHeap.add(node.next);
+            }
+        }
+
         return dummy.next;
     }
-
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode head = null;
-        ListNode tail = null;
-
-        for (int i = 0; i < lists.length; i++) {
-            ListNode temp = lists[i];
-            while (temp != null) {
-                ListNode newNode = new ListNode(temp.val);
-                if (head == null) {
-                    head = tail = newNode;
-                } else {
-                    tail.next = newNode;
-                    tail = tail.next;
-                }
-                temp = temp.next;
-            }
-        }
-
-        return sort(head);
-    }
 }
-
