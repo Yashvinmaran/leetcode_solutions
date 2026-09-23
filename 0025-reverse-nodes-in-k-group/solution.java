1f01e4577ea5e5bc22a40171d313c9d1aa0ea5cc
Yashvin Maran <155080769+Yashvinmaran@users.noreply.github.com>
2026-09-23 13:35:23 +0000

    Sync LeetCode submission Runtime - 1 ms (33.87%), Memory - 46.1 MB (94.00%)

--- 0025-reverse-nodes-in-k-group/solution.java
+++ 0025-reverse-nodes-in-k-group/solution.java
@@ -1,34 +1,50 @@
-class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k == 1) return head;
+class Solution { 
 
-        int length = 0;
-        ListNode temp = head;
-        while (temp != null) { 
-            length++;
-            temp = temp.next;
-        }
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
+    public ListNode reverseBetween(ListNode head, int left, int right) { 
+        ListNode start = head; 
+        ListNode end = head; 
+        ListNode dummy = new ListNode(0); 
+        dummy.next = head; 
+        ListNode prev1 = dummy; 
         
-        while (length >= k) {
-            ListNode curr = prev.next;
-            ListNode agla = curr.next;
-            
-            for (int i = 1; i < k; i++) {
-                curr.next = agla.next;
-                agla.next = prev.next;
-                prev.next = agla;
-                agla = curr.next;
-            }
+        for (int i = 1; i <= right; i++){ 
+            if(i < left){ 
+                prev1 = start; 
+                start = start.next; 
+            } 
+            if(i < right){ 
+                end = end.next; 
+            } 
+        } 
+        ListNode next1 = end.next; 
+        ListNode prev = null, next = null; 
+        ListNode temp = start; 
+        while(start != next1){ 
+            next = start.next; 
+            start.next = prev; 
+            prev = start; 
+            start = next; 
+        } 
+        prev1.next = prev; 
+        temp.next = next1; 
+        
+        return dummy.next; 
+    } 
 
-            prev = curr;
-            length -= k;
-        }
-
-        return dummy.next;
-    }
+    public ListNode reverseKGroup(ListNode head, int k) { 
+        int cnt = 0; 
+        ListNode temp = head; 
+        
+        while(temp != null){ 
+            cnt++; 
+            temp = temp.next; 
+        } 
+        
+        for (int i = 1; i + k - 1 <= cnt; i += k) {
+            head = reverseBetween(head, i, i + k - 1); 
+        } 
+        
+        return head; 
+    } 
 }
 
